3d4ffdb562d9e4ecf41a33d902c6dade38fe7544
aleeraza12 <39170898+aleeraza12@users.noreply.github.com>
2020-07-19 09:38:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Message } from './Message'
+import { Message } from './Message' //{}braces is lye k default export nhi kia message app me
 function App() {
   let [count, setCount] = useState(1);
   return (
