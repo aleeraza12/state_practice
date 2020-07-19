2dff0c7594e9c76cbb940a3c476147b85a42a036
aleeraza12 <39170898+aleeraza12@users.noreply.github.com>
2020-07-19 09:55:58 +0500

    Add a css file

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import { Message } from './Message' //{}braces is lye k default export nhi kia message app me
 function App() {
   let [count, setCount] = useState(1);
+  let [isMorning, setMorning] = useState(false);
+
   return (
 
-    <div>
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+      <h2>Day time = {isMorning ? 'Morning' : 'Night'}</h2>
       <Message counter={count} />
       <br />
       <button onClick={
