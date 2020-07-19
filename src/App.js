3611cbd4bae3c12e36e105c186d116c51d8def82
aleeraza12 <39170898+aleeraza12@users.noreply.github.com>
2020-07-19 09:37:18 +0500

    add button to click and to change in state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import { Message } from './Message'
 function App() {
+  let [count, setCount] = useState(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      <Message counter={count} />
+      <br />
+      <button onClick={
+        () => setCount(count + 1)
+      }>
+        Click Me
+      </button>
+
+
     </div>
   );
 }
